71732ffe7f11d8e8fc44b2300efd351d3c581f2b
Cartaloop <lucasrech00@gmail.com>
2024-06-03 00:18:59 -0300

    Create Service layer

--- src/main/java/edu/lucasrech/todo_list/services/TaskService.java
+++ src/main/java/edu/lucasrech/todo_list/services/TaskService.java
@@ -1,4 +1,14 @@
 package edu.lucasrech.todo_list.services;
 
+import edu.lucasrech.todo_list.repository.TaskRepository;
+
 public class TaskService {
+
+    TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
 }
